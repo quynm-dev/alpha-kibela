ed28e0d08cc47623a03087010db1d11dd7dca203
stark-mfx <nguyen.minh.quy.a@moneyforward.co.jp>
2024-08-01 17:05:48 +0900

    Add user entity

---
+++ src/main/kotlin/alpha/data/entity/UserEntity.kt
@@ -0,0 +1,26 @@
+package alpha.data.entity
+
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.javatime.CurrentDateTime
+import org.jetbrains.exposed.sql.javatime.datetime
+
+class UserEntity(id: EntityID<Int>): IntEntity(id) {
+    companion object: IntEntityClass<UserEntity>(Users)
+
+    var username by Users.username
+    var email by Users.email
+    var password by Users.password
+    var createdAt by Users.createdAt
+    var updatedAt by Users.updatedAt
+}
+
+object Users: IntIdTable("users") {
+    val username = varchar("username", 50)
+    val email = varchar("email", 100).uniqueIndex()
+    val password = varchar("password", 255)
+    val createdAt = datetime("created_at").defaultExpression(CurrentDateTime)
+    val updatedAt = datetime("updated_at").defaultExpression(CurrentDateTime)
+}
