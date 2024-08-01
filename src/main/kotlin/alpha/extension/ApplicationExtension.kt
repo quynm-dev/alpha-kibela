60eeda63a990729f609aa8e930bb2bdd5fe2979d
stark-mfx <nguyen.minh.quy.a@moneyforward.co.jp>
2024-08-01 23:06:00 +0900

    Add param parser

---
+++ src/main/kotlin/alpha/extension/ApplicationExtension.kt
@@ -0,0 +1,36 @@
+package alpha.extension
+
+import io.ktor.server.application.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.MissingFieldException
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.*
+import mu.KotlinLogging
+
+val logger = KotlinLogging.logger {}
+
+@OptIn(ExperimentalSerializationApi::class)
+inline fun <reified T: Any> ApplicationCall.receiveParams(): T {
+    val j = Json {
+        ignoreUnknownKeys = true
+    }
+    val paramObj = buildJsonObject {
+        parameters.forEach { key, values ->
+            if(key.endsWith("[]")) {
+                putJsonArray(key.dropLast(2)) { addAll(values) }
+            } else if (values.isNotEmpty()) {
+                put(key, values.first())
+            }
+        }
+    }
+
+    return try {
+        j.decodeFromJsonElement(paramObj)
+    } catch (e: MissingFieldException) {
+        logger.error { e.message }
+        throw e
+    } catch (e: SerializationException) {
+        logger.error { e.message }
+        throw e
+    }
+}
