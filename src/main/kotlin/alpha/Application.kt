e6b8418fd9a7f1f8103348ee471e22ad0c4a44f8
stark-mfx <nguyen.minh.quy.a@moneyforward.co.jp>
2024-08-01 15:05:45 +0900

    Initial commit

---
+++ src/main/kotlin/alpha/Application.kt
@@ -0,0 +1,12 @@
+package alpha
+
+import alpha.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
